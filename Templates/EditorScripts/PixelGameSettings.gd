3a39da6fb1eaaa6aeb34c9e3e8fd5227a7cc7168
MeanMo <140993349+Yanndri@users.noreply.github.com>
2025-02-07 01:44:37 +0800

    new update from main

--- Templates/EditorScripts/PixelGameSettings.gd
+++ Templates/EditorScripts/PixelGameSettings.gd
@@ -6,12 +6,12 @@
 #Note: 640x360 resolution scales well to 720, 1080, and 1440.
 func _run() -> void:
 	#Size of the viewport
-	ProjectSettings.set("display/window/size/viewport_width", 320)
-	ProjectSettings.set("display/window/size/viewport_height", 180)
+	ProjectSettings.set("display/window/size/viewport_width", 180)
+	ProjectSettings.set("display/window/size/viewport_height", 320)
 	
 	#Size of the window
-	ProjectSettings.set("display/window/size/window_width_override", 640)
-	ProjectSettings.set("display/window/size/window_height_override", 360)
+	ProjectSettings.set("display/window/size/window_width_override", 360)
+	ProjectSettings.set("display/window/size/window_height_override", 640)
 	
 	#Layer Names
 	ProjectSettings.set("layer_names/2d_physics/layer_1", "Player")
