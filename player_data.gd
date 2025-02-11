1eaf846daf1b0e49e7164d42a78628e2b867f5b2
MeanMo <140993349+Yanndri@users.noreply.github.com>
2025-02-11 13:25:02 +0800

    Spawner Updated
    The spawner now only accepts cells that the owner has, and when a cell from a pop comes in contact, the color changes, also added player_data

--- player_data.gd
+++ player_data.gd
@@ -1,4 +1,10 @@
 extends Node
 
-var players = []
-var current_player : int 
+var players = [] #holds the amount of players and there corresponding color, received from $Control/Player_Display
+var current_player : int : #index of the current player, changes upon pressing a grid from cell_spawner script
+	set(value):
+		previous_player = current_player #assign the current player value to the previous player.
+		current_player = value #change the current player to the new value
+		if current_player > players.size() - 1: #if the value surpasses the players size, go back to 0
+			current_player = 0
+var previous_player : int #used to check who was the last player that pressed the spawner
