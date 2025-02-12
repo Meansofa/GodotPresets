24f0ee9f2c1a3d20e48bea0dc3a76ddc74f1860c
MeanMo <140993349+Yanndri@users.noreply.github.com>
2025-02-13 02:00:01 +0800

    Count how many cells a player has

--- player_data.gd
+++ player_data.gd
@@ -1,6 +1,7 @@
 extends Node
 
-var players = [] #holds the amount of players and there corresponding color, received from $Control/Player_Display
+#Check the Player_Display Control node to see how these are used
+var players = [] #holds the amount of players and there corresponding color
 var current_player : int : #index of the current player, changes upon pressing a grid from cell_spawner script
 	set(value):
 		previous_player = current_player #assign the current player value to the previous player.
@@ -8,6 +9,24 @@ var current_player : int : #index of the current player, changes upon pressing a
 		if current_player > players.size() - 1: #if the value surpasses the players size, go back to 0
 			current_player = 0
 		emit_signal("player_change")
+		
 var previous_player : int #used to check who was the last player that pressed the spawner
 
-signal player_change
+var players_cell_count : Dictionary #how many cells each registered players have
+
+signal player_change #emit signal everytime the current player has changed
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		print("players_playing> ", players_cell_count)
+
+func calculate_cell_count(player : int, value : int): #called everytime a cell is added or removed from a player's cell
+	if player == -1: #-1 means there is no player
+		return
+	if not players_cell_count.has(player): #check if this player is not yet registered(player have already put their first cell)
+		players_cell_count[player] = 1 #if not registered, register the player by adding it's index in the array 
+		print("Player ", player + 1, " registered!")
+		return
+		
+	players_cell_count[player] += value #add or remove a cell to the player's cell count
+	print("players_playing> ", players_cell_count)
