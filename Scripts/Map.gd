d2a0bc0c6243eec4b2755c10125312704e6bd9b6
MeanMo <140993349+Yanndri@users.noreply.github.com>
2025-03-05 15:21:00 +0800

    Map fix
    Map fixed but now only even Vectors, but is now always centered

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,110 +1,156 @@
 extends TileMapLayer
 
-var default_column_size := 8
-var default_row_size := 4
-var columns := 8 :
+var columns := 0 :
 	set(value):
 		columns = value
 		emit_signal("map_size_changed", columns, rows)
-var rows := 4 :
+var rows := 0 :
 	set(value):
 		rows = value
 		emit_signal("map_size_changed", columns, rows)
+
+var top_row : int = 0
+var bottom_row : int = 0
+var left_column : int = 0
+var right_column : int = 0
 
 var last_value : int
 
 signal map_size_changed
 
-#func _input(event: InputEvent) -> void:
-	#if Input.is_action_just_pressed("ui_right"):
-		#_add_columns(1)
-	#if Input.is_action_just_pressed("ui_down"):
-		#_add_rows(1)
-	#if Input.is_action_just_pressed("ui_accept"):
-		#_remove_columns(1)
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_right"):
+		_add_column()
+	if Input.is_action_just_pressed("ui_left"):
+		_remove_column()
+	if Input.is_action_just_pressed("ui_up"):
+		_add_row() 
+	if Input.is_action_just_pressed("ui_down"):
+		_remove_row()
 
 func _ready() -> void:
-	_generate_grid()
-	pass
+	_reset_grid()
+	#_generate_grid(4, 2)
 
-func _generate_grid():
-	for column in columns:
-		for row in rows:
-			var pos := Vector2i(column + 1, row + 1)
-			set_cell(pos, 0, Vector2i(0, 0), 1)
+func _reset_grid():
+	clear()
+	top_row = 0
+	bottom_row = 0
+	left_column = 0
+	right_column = 0
+	columns = 0
+	rows = 0
 
-func _add_columns(additional_columns : int):
-	for column in additional_columns:
-		columns += 1
-		for row in rows:
-			var pos := Vector2i(columns, row + 1)
-			set_cell(pos, 0, Vector2i(0, 0), 1)
-		await get_tree().create_timer(0.05).timeout
-	print(self.name, ">columns: ", columns)
+func _generate_grid(add_columns : int, add_rows : int):
+	for column in range(add_columns):
+		_add_column()
+	for row in range(add_rows):
+		_add_row()
+	print("columns: ", columns, " rows: ", rows)
 
-func _add_rows(additional_rows : int):
-	for row in additional_rows:
-		rows += 1
-		for column in columns:
-			var pos := Vector2i(column + 1, rows)
-			set_cell(pos, 0, Vector2i(0, 0), 1)
-		await get_tree().create_timer(0.05).timeout
-	print(self.name, ">rows: ", rows)
-
-func _remove_rows(removed_rows : int):
-	if rows <= 0:
-		return
-	for row in removed_rows:
-		for column in columns:
-			var pos := Vector2i(column + 1, rows)
-			erase_cell(pos)
-		rows -= 1
-	print(self.name, ">rows: ", rows)
-
-func _remove_columns(removed_columns : int):
-	if columns <= 0:
-		return
-	for column in removed_columns:
-		for row in rows:
-			var pos := Vector2i(columns, row + 1)
-			erase_cell(pos)
-		columns -= 1
-	print(self.name, ">columns: ", columns)
-
-
-func _on_map_size_value_changed(value: float) -> void:
-	print("value: ", value)
-	if value > last_value:
-		position += Vector2(value, value)
-		var additional_columns = ceili(value / 100.0 * default_column_size)
-		var additional_rows = ceili(value / 100.0 * default_row_size)
-		if value >= 60:
-			if value >= 80:
-				additional_columns = ceili(300 / 100.0 * default_column_size)
-				additional_rows = ceili(300 / 100.0 * default_row_size)
-			else:
-				additional_columns = ceili(75 / 100.0 * default_column_size)
-				additional_rows = ceili(75 / 100.0 * default_row_size)
-		_add_columns(additional_columns)
-		_add_rows(additional_rows)
-	if value < last_value:
-		position -= Vector2(last_value, last_value)
-		var additional_columns = ceili(last_value / 100.0 * default_column_size)
-		var additional_rows = ceili(last_value / 100.0 * default_row_size)
-		if last_value >= 60:
-			if last_value >= 80:
-				additional_columns = ceili(300 / 100.0 * default_column_size)
-				additional_rows = ceili(300 / 100.0 * default_row_size)
-			else:
-				additional_columns = ceili(75 / 100.0 * default_column_size)
-				additional_rows = ceili(75 / 100.0 * default_row_size)
-		_remove_columns(additional_columns)
-		_remove_rows(additional_rows)
-	if value != last_value:
-		last_value = value
+func _add_column(): #add left and right cells
+	for row in range(bottom_row): #starts at 0 index
+		var bottom_right_pos = Vector2i(right_column, row)
+		set_cell(bottom_right_pos, 0, Vector2i(0, 0), 1)
+		
+		var bottom_left_pos = Vector2i(-left_column - 1, row)
+		set_cell(bottom_left_pos, 0, Vector2i(0, 0), 1)
 	
-	print(self.name, "Map Size: ", position)
+	for row in range(top_row + 1): #top row is negative so need to start at -1 hence top_row + 1
+		var top_right_pos = Vector2i(right_column, -row)
+		set_cell(top_right_pos, 0, Vector2i(0, 0), 1)
+		
+		var top_left_pos = Vector2i(-left_column - 1, -row)
+		set_cell(top_left_pos, 0, Vector2i(0, 0), 1)
+	
+	right_column += 1
+	left_column += 1
+	columns += 2
+	
+func _add_row(): #add top and bottom cells
+	for column in range(right_column):  #starts at 0 index
+		var bottom_right_pos = Vector2i(column, bottom_row)
+		set_cell(bottom_right_pos, 0, Vector2i(0, 0), 1)
+		
+		var bottom_left_pos = Vector2i(-column - 1, bottom_row)
+		set_cell(bottom_left_pos, 0, Vector2i(0, 0), 1)
+	
+	for column in range(left_column): #left_column is negative so need to start at -1 hence top_row + 1
+		var top_right_pos = Vector2i(column, -top_row - 1)
+		set_cell(top_right_pos, 0, Vector2i(0, 0), 1)
+		
+		var top_left_pos = Vector2i(-column - 1, -top_row - 1)
+		set_cell(top_left_pos, 0, Vector2i(0, 0), 1)
 
+	top_row += 1
+	bottom_row += 1
+	rows += 2
+
+func _remove_column():
+	right_column -= 1
+	left_column -= 1
+	columns -= 2
+	for row in range(bottom_row): #starts at 0 index
+		var bottom_right_pos = Vector2i(right_column, row)
+		erase_cell(bottom_right_pos)
+		
+		var bottom_left_pos = Vector2i(-left_column - 1, row)
+		erase_cell(bottom_left_pos)
+	
+	for row in range(top_row + 1): #top row is negative so need to start at -1 hence top_row + 1
+		var top_right_pos = Vector2i(right_column, -row)
+		erase_cell(top_right_pos)
+		
+		var top_left_pos = Vector2i(-left_column - 1, -row)
+		erase_cell(top_left_pos)
+
+func _remove_row():
+	top_row -= 1
+	bottom_row -= 1
+	rows -= 2
+	for column in range(right_column):  #starts at 0 index
+		var bottom_right_pos = Vector2i(column, bottom_row)
+		erase_cell(bottom_right_pos)
+		
+		var bottom_left_pos = Vector2i(-column - 1, bottom_row)
+		erase_cell(bottom_left_pos)
+		
+	for column in range(left_column): #left_column is negative so need to start at -1 hence top_row + 1
+		var top_right_pos = Vector2i(column, -top_row - 1)
+		erase_cell(top_right_pos)
+		
+		var top_left_pos = Vector2i(-column - 1, -top_row - 1)
+		erase_cell(top_left_pos)
+
+func _on_map_size_value_changed(value: int) -> void:
+	print(self.name, ">value: ", value, " last value: ", last_value)
+	if value == last_value:
+		return
+	_reset_grid()
+	
+	
+	var additional_columns : int
+	var additional_rows : int
+	match value:
+		0: 
+			additional_columns = 4
+			additional_rows = 2
+		20:
+			additional_columns = 5
+			additional_rows = 2
+		40:
+			additional_columns = 7
+			additional_rows = 3
+		60:
+			additional_columns = 10
+			additional_rows = 5
+		80:
+			additional_columns = 22
+			additional_rows = 11
+	
+	_generate_grid(additional_columns, additional_rows)
+	
+	last_value = value
 
 func get_available_directions(position : Vector2) -> Array: #called from the spawner for them to check their neighbors
 
